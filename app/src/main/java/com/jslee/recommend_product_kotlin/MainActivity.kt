c4b4c3186ba186ee07a860a7f3944e70f414e6aa
jslee314 <jslee_314@naver.com>
2021-04-11 21:21:59 +0900

    Option menu 만들기

--- app/src/main/java/com/jslee/recommend_product_kotlin/MainActivity.kt
+++ app/src/main/java/com/jslee/recommend_product_kotlin/MainActivity.kt
@@ -1,18 +1,64 @@
 package com.jslee.recommend_product_kotlin
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var navController : NavController
+    val SETTING_ACTIVITY = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        navController = nav_host_fragment.findNavController()
+//        navController = nav_host_fragment.findNavController()
+
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        Log.d("jslee", "onCreateOptionsMenu")
+        // XML로 메뉴를 구성
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+       // 메뉴의 id 별로 분기한다.
+        when(item.itemId){
+            R.id.setting_item -> {
+
+                val setting_Intent = Intent(this, SettingActivity::class.java)
+                // startActivity(second_intent)
+                startActivityForResult(setting_Intent, SETTING_ACTIVITY)
+            }
+            R.id.geo_item -> {
+                val uri = Uri.parse("geo:37.243243,131.861601")
+                val intent = Intent(Intent.ACTION_VIEW, uri)
+                startActivity(intent)
+            }
+            R.id.https_item -> {
+                val uri = Uri.parse("https://jade314.tistory.com/")
+                val intent = Intent(Intent.ACTION_VIEW, uri)
+                startActivity(intent)
+            }
+            R.id.tel_item -> {
+                val uri = Uri.parse("tel:12341234")
+                val intent = Intent(Intent.ACTION_DIAL, uri)
+                startActivity(intent)
+            }
+            R.id.item3 -> {
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
 }
