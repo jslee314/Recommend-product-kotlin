81aabe910a59b12206245e13d115d9aec390510a
jslee314 <jslee_314@naver.com>
2021-04-15 22:37:09 +0900

    - 파일명 수정

--- app/src/main/java/com/jslee/recommend_product_kotlin/fragment/TextUIFragment.kt
+++ app/src/main/java/com/jslee/recommend_product_kotlin/fragment/TextUIFragment.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [QizeFragment.newInstance] factory method to
+ * Use the [TextEditFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class QizeFragment : Fragment() {
+class TextEditFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -30,12 +30,10 @@ class QizeFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_qize, container, false)
+        return inflater.inflate(R.layout.fragment_text_ui, container, false)
     }
 
     companion object {
@@ -45,16 +43,16 @@ class QizeFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment QizeFragment.
+         * @return A new instance of fragment TextEditFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            QizeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                TextEditFragment().apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_PARAM1, param1)
+                        putString(ARG_PARAM2, param2)
+                    }
                 }
-            }
     }
 }
